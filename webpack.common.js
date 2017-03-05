e94019c6588205cbf586f50af501afe9a562b878
priyankaaraigmail <priyanka.b.rai@gmail.com>
2017-03-05 22:02:31 +0530

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: {
     'polyfills': './src/polyfills.ts',
     'vendor': './src/vendor.ts',
-    'app': './src/app/app.module.ts'
+    'app': './src/main.ts'
   },
 
   resolve: {
